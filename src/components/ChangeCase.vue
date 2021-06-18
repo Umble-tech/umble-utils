08d7320e164ce4e4967a0a50e62d3a7c6937a077
Tobias Wulvik <tobias.wulvik@gmail.com>
2021-06-18 12:03:37 +0200

    textarea

---
+++ src/components/ChangeCase.vue
@@ -0,0 +1,32 @@
+<template>
+  <textarea
+    name=""
+    id=""
+    cols="30"
+    rows="10"
+    v-model="text"
+    @keyup="toggleCase"
+  ></textarea>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+      toggled: false,
+    };
+  },
+  methods: {
+    toggleCase(event) {
+      if (event.key === "Escape" || event.key === "CapsLock") {
+        this.text = this.toggled
+          ? this.text.toLowerCase()
+          : this.text.toUpperCase();
+        this.toggled = !this.toggled;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped></style>
